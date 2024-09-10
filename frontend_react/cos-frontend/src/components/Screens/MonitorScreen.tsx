03098becd3bdde8b6840e60a45e05b14fd1db725
reticent-roklimber <dev.equinoct@gmail.com>
2024-09-10 14:23:52 +0530

    Move MonitoringScreen.tsx from 'components' to 'Screens' within 'components'

--- frontend_react/cos-frontend/src/components/Screens/MonitorScreen.tsx
+++ frontend_react/cos-frontend/src/components/Screens/MonitorScreen.tsx
@@ -1,8 +1,20 @@
-import React from "react";
-import AlertWidgetComponent from "../AlertWidget/AlertWidgetComponent";
-import MenuList from "../Menu1/MenuList";
-import Map from "../Maps/MonitoringMap.js";
-import StartButton from "./ResponseScreen";
+import React, {useState} from "react";
+import AlertWidgetComponent from "./AlertWidget/AlertWidgetComponent";
+import MenuList from "./Menu/MenuList";
+import LayerComponent from "./LayerWidget/LayersComponent";
+import NavComponent from "./NavBar/NavComponent";
+import Legend from "./LegendWidget/Legend";
+
+type Gaugetype = "rainfall" | "reservoir" | "tidal" | "groundwater" | "riverWater" | "regulators";
+
+const initialVisibleGauges: Record<Gaugetype, boolean> = {
+  rainfall: false,
+  reservoir: false,
+  tidal: false,
+  groundwater: false,
+  riverWater: false,
+  regulators: false,
+};
 
 const MonitorScreen: React.FC = () => {
   {
@@ -14,12 +26,31 @@ const MonitorScreen: React.FC = () => {
     throw new Error("Function not implemented.");
   }
 
+  const [visibleGauges, setVisibleGauges] = useState(initialVisibleGauges);
+
+  const toggleGauge = (gauge: Gaugetype) => {
+    setVisibleGauges((prevState) => ({
+      ...prevState,
+      [gauge]: !prevState[gauge],
+    }));
+  };
+
   return (
-    <div className="monitor-screen mx-auto">
-      <Map/>
-      <MenuList/>
-      <AlertWidgetComponent/>
-      {/* <StartButton/> */}
+    <div className="monitor-screen w-full h-[900px] bg-gray-100 mx-auto relative flex flex-col">
+      <div style={{position:"absolute", left: "0px"}}>
+        <MenuList/>
+      </div>
+      <div className="w-full">
+        <NavComponent/>
+      </div>
+
+      <div className="absolute top-0 right-0 mt-[90px] mr-[20px] flex flex-col items-end">
+        <LayerComponent visibleGauges={visibleGauges} toggleGauge={toggleGauge}/>
+        <div className="mt-[15px] right-0"> <Legend visibleGauges={visibleGauges}/> </div>
+      </div>
+      <div style={{ position: "absolute", top: "200px", left: "111px"}}>
+        <AlertWidgetComponent/>
+      </div>
     </div>
   );
 };
