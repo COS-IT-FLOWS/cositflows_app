20f068e12a6ba7b66b803fc4082b8479b5cb0510
reticent-roklimber <dev.equinoct@gmail.com>
2024-09-12 17:30:47 +0530

    + Modify src/index.tsx to correct the file location from   'Components/MonitorScreen' to 'components/MonitorScreen'

--- frontend_react/cos-frontend/src/index.tsx
+++ frontend_react/cos-frontend/src/index.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import MonitorScreen from './Components/MonitorScreen';
+import MonitorScreen from './components/MonitorScreen';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
