5f7aae9718f2d99493d3cd408446002f7a491125
reticent-roklimber <dev.equinoct@gmail.com>
2024-09-15 12:41:48 +0530

    Add registration, login to django backend

---
+++ backend_django/cositflows_app/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from .models import CustomUser
+
+class RegistrationForm(forms.ModelForm):
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
+
+    class Meta:
+        model = CustomUser
+        fields = ('email',)
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords don't match")
+        return password2
+
+class LoginForm(forms.Form):
+    email = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput)
